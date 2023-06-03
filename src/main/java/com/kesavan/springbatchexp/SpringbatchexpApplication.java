c27ad00ea9485b17828a6a0840385f7bebc2499e
kesavanmurali <k7mraj@gmail.com>
2023-06-03 12:26:24 +0530

    Initialization - Spring batch maven project

---
+++ src/main/java/com/kesavan/springbatchexp/SpringbatchexpApplication.java
@@ -0,0 +1,13 @@
+package com.kesavan.springbatchexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchexpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kesavan/springbatchexp/SpringbatchexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kesavan.springbatchexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchexpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
